0bf33d1e399aaf8e800a6622d77ce9a496c6a5d7
Yuriy Zubarev <yuriyzubarev7@gmail.com>
2011-09-08 19:17:10 -0700

    setting up text fixture in setUp method

--- test/domain/test_twitter.py
+++ test/domain/test_twitter.py
@@ -3,22 +3,22 @@ from twitter import Twitter
 from mock_twitter_fetcher import MockTwitterFetcher
 
 class MyTestCase(unittest.TestCase):
+    
+    def setUp(self):
+        self.twitter = Twitter(MockTwitterFetcher())
 
     def test_lists_for_user(self):
-        twitter = Twitter(MockTwitterFetcher())
-        slugs = twitter.get_lists("bob")
+        slugs = self.twitter.get_lists("bob")
         self.assertTrue("software-development" in slugs)
 
     def test_members_for_owner_screen_name_and_slug(self):
-        twitter = Twitter(MockTwitterFetcher())
-        members = twitter.get_members("bob", "family")
+        members = self.twitter.get_members("bob", "family")
         self.assertTrue("rebeccaparsons" in members)
         self.assertTrue("mpoppendieck" in members)
         self.assertTrue("postwait" in members)
 
     def test_member_subscriber_counts(self):
-        twitter = Twitter(MockTwitterFetcher())
-        m, s = twitter.get_member_subscriber_counts("bob", "53257043")
+        m, s = self.twitter.get_member_subscriber_counts("bob", "53257043")
         self.assertEqual(25, m)
         self.assertEqual(0, s)
 
