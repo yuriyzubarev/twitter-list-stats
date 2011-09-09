e7efe03d385292a90972ecd2e486481fbb0097c9
Yuriy Zubarev <yuriyzubarev7@gmail.com>
2011-09-09 13:15:07 -0700

    SnapShot entity and its association with TList

--- entities.py
+++ entities.py
@@ -4,4 +4,8 @@ class TList(db.Model):
     id = db.StringProperty(required = True)
     slug = db.StringProperty(required = True)
     
-    
+class SnapShot(db.Model):
+    tlist = db.ReferenceProperty(TList, collection_name = "snap_shots")
+    created_on = db.DateTimeProperty(auto_now_add = True)
+    members = db.IntegerProperty()
+    subscribers = db.IntegerProperty()
--- test/domain/test_storage.py
+++ test/domain/test_storage.py
@@ -1,6 +1,8 @@
 import unittest
 from google.appengine.ext import testbed
 from entities import TList
+from entities import SnapShot
+from datetime import datetime
 
 class StorageTestCase(unittest.TestCase):
     
@@ -19,4 +21,29 @@ class StorageTestCase(unittest.TestCase):
         self.assertEqual("123", results[0].id)
         self.assertEqual("slug123", results[0].slug)
          
-         
+    def test_create_snapshot(self):
+        tlist = TList(id = "123", slug = "slug123")
+        tlist.put()
+        
+        created_on = datetime.now()
+        
+        snap = SnapShot(members = 11, subscribers = 23)
+        snap.tlist = tlist
+        snap.created_on = created_on
+        snap.put()
+
+        results = SnapShot.all().fetch(2)
+        self.assertEqual(1, len(results))
+        self.assertEqual(11, results[0].members)
+        self.assertEqual(23, results[0].subscribers)
+        self.assertEqual(created_on, results[0].created_on)
+        self.assertEqual("123", results[0].tlist.id)
+        
+        #test back-reference
+        results = TList.all().fetch(2)
+        self.assertEqual(1, len(results))
+        self.assertEqual(1, results[0].snap_shots.count())
+        self.assertEqual(11, results[0].snap_shots[0].members)
+        
+
+
