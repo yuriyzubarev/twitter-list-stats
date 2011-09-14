9fbe4f48bd91f6d54ba7cd7db1cbbae3cdfe1885
Yuriy Zubarev <yuriyzubarev7@gmail.com>
2011-09-13 21:34:37 -0700

    user_screen_name for TList and members for SnapShot

--- src/entities.py
+++ src/entities.py
@@ -3,9 +3,11 @@ from google.appengine.ext import db
 class TList(db.Model):
     id = db.StringProperty(required = True)
     slug = db.StringProperty(required = True)
+    user_screen_name = db.StringProperty(required = True)
     
 class SnapShot(db.Model):
     tlist = db.ReferenceProperty(TList, collection_name = "snap_shots")
     created_on = db.DateTimeProperty(auto_now_add = True)
-    members = db.IntegerProperty()
-    subscribers = db.IntegerProperty()
+    members_count = db.IntegerProperty()
+    subscribers_count = db.IntegerProperty()
+    members = db.StringListProperty()
--- test/repositories_test.py
+++ test/repositories_test.py
@@ -13,29 +13,33 @@ class StorageTestCase(unittest.TestCase):
         self.testbed.init_datastore_v3_stub()
      
     def test_create_list(self):
-        tlist = TList(id = "123", slug = "slug123")
+        tlist = TList(id = "123", slug = "slug123", user_screen_name = "yuriy_zubarev")
         tlist.put()
         
         results = TList.all().fetch(2)
         self.assertEqual(1, len(results))
         self.assertEqual("123", results[0].id)
         self.assertEqual("slug123", results[0].slug)
+        self.assertEqual("yuriy_zubarev", results[0].user_screen_name)
          
     def test_create_snapshot(self):
-        tlist = TList(id = "123", slug = "slug123")
+        tlist = TList(id = "123", slug = "slug123", user_screen_name = "yuriy_zubarev")
         tlist.put()
         
         created_on = datetime.now()
         
-        snap = SnapShot(members = 11, subscribers = 23)
+        snap = SnapShot(members_count = 11, subscribers_count = 23)
         snap.tlist = tlist
         snap.created_on = created_on
+        snap.members = ["mpoppendieck", "rebeccaparsons"]
         snap.put()
 
         results = SnapShot.all().fetch(2)
         self.assertEqual(1, len(results))
-        self.assertEqual(11, results[0].members)
-        self.assertEqual(23, results[0].subscribers)
+        self.assertEqual(11, results[0].members_count)
+        self.assertTrue("mpoppendieck" in results[0].members)
+        self.assertTrue("rebeccaparsons" in results[0].members)
+        self.assertEqual(23, results[0].subscribers_count)
         self.assertEqual(created_on, results[0].created_on)
         self.assertEqual("123", results[0].tlist.id)
         
@@ -43,7 +47,7 @@ class StorageTestCase(unittest.TestCase):
         results = TList.all().fetch(2)
         self.assertEqual(1, len(results))
         self.assertEqual(1, results[0].snap_shots.count())
-        self.assertEqual(11, results[0].snap_shots[0].members)
+        self.assertEqual(11, results[0].snap_shots[0].members_count)
         
 
 
