4f5d8d8834525df135a3a61ac6e2307ca9b85596
Yuriy Zubarev <yuriyzubarev7@gmail.com>
2011-10-25 11:40:54 -0700

    Refactoring again. Providing docstrings and renaming variables. It is good to have a fresh perspective from a break. It would also be nice to have some diagrams otherwise a comprehension of the code is going to be more time consuming and difficult as time goes by.

--- src/services.py
+++ src/services.py
@@ -6,26 +6,37 @@ import urllib2
 import json
 
 class Twitter():
+
+    """ 
+    Service responsible for translating responses from twitter.com
+    into application domain constructs.
+    """
     
     def __init__(self, twitter_fetcher):
         self.fetcher = twitter_fetcher
         
-    def get_lists(self, user):
-        json_response = self.fetcher.get_lists_json(user)
-        slugs = []
+    def get_lists(self, owner_screen_name):
+        """ Returns a list of tuples of 'list id' and 'slug' for a given owner. """
+
+        json_response = self.fetcher.get_lists_json(owner_screen_name)
+        tlists = []
         for l in json_response["lists"]:
-            slugs.append((l["id_str"], l["slug"]))
-        return slugs
+            tlists.append((l["id_str"], l["slug"]))
+        return tlists
     
-    def get_members(self, owner_screen_name, slug):
-        json_response = self.fetcher.get_members_json(owner_screen_name, slug)
+    def get_members(self, owner_screen_name, tlist_slug):
+        """ Returns a list of screen names of all the members for a given owner and slug. """
+
+        json_response = self.fetcher.get_members_json(owner_screen_name, tlist_slug)
         screen_names = []
         for user in json_response["users"]:
             screen_names.append(user["screen_name"])
         return screen_names
         
-    def get_member_subscriber_counts(self, user, list_id):
-        json_response = self.fetcher.get_list_json(user, list_id)
+    def get_member_subscriber_counts(self, owner_screen_name, tlist_id):
+        """ Returns a tuple of member count for a given owner and list id. """
+
+        json_response = self.fetcher.get_list_json(owner_screen_name, tlist_id)
         members = json_response["member_count"]
         subscriber = json_response["subscriber_count"]
         return members, subscriber
